a0973e8a025dd312402ddd69839af7a730583c2d
JPy <jparis.py@gmail.com>
2018-04-09 13:29:29 +0200

    test wsgi for deploy with gunicorn

--- cis/wsgi.py
+++ cis/wsgi.py
@@ -5,3 +5,24 @@ import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
+from app import app
+
+
+if __name__ == '__main__':
+	""" 
+	runner for the CIS front Flask app 
+	- gets most of its variables at start from environment variables
+	- 
+
+	in command line just type : 
+	"python run_cis_front.py"
+
+	"""
+
+	print "= "*25
+	print "= = = WSGI / RERUN FLASK APP = = ="
+	print "= "*25
+
+	# simple flask runner
+	app.run( )
