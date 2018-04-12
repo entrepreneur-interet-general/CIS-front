e452fa707e02e75dd7a47698cf7071d1d05131d7
JPy <jparis.py@gmail.com>
2018-04-12 13:36:40 +0200

    cleaning some slices in landing + first steps in vue for search engine

--- cis/app/static/js/cis-custom-vue.js
+++ cis/app/static/js/cis-custom-vue.js
@@ -6,30 +6,40 @@
 // following tutorial for now...
 // cf : https://fr.vuejs.org/v2/guide/
 
+Vue.config.delimiters =  ['[[',']]'] ;
+
+
+
+
 Vue.component('todo-item', {
 	// Le composant todo-item accepte maintenant une
 	// « prop » qui est comme un attribut personnalisé.
 	// Cette prop est appelée todo.
+
+	delimiters: ['[[',']]'],
+
 	props: ['todo'],
-	template: '<li>{{ todo.text }}</li>'
+	template: '<li>[[ todo.text ]]</li>'
 })
 
 
 
 
-var app = new Vue({
+var app_test = new Vue({
 
-	el: '#vue_test',
+	el: '#vue_test', //'#user-switch',
 	delimiters: ['[[',']]'],
 	
 	data: {
-		message		: 'Bienvenue in vue.js ...',
-		seen		: false,
-		mess_hover 	: 'Bienvenue...' + new Date().toLocaleString(),
-		todos		: [
+		message			: 'Bienvenue in vue.js ...',
+		results_count	: 678,
+		seen			: false,
+		mess_hover 		: 'Bienvenue...' + new Date().toLocaleString(),
+		todos			: [
 			{ text: 'Apprendre JavaScript' },
 			{ text: 'Apprendre Vue' },
-			{ text: 'Créer quelque chose de génial' }
+			{ text: 'Intégrer Vue au moteur de recherche' },
+			{ text: 'Passer à la prochaine app' }
 			]
 	},
 	
@@ -41,3 +51,16 @@ var app = new Vue({
 
 
 })
+
+
+// var app = new Vue({
+
+// 	el: '#cis_app',
+// 	delimiters: ['[[',']]'],
+	
+// 	data: {
+// 		results_count		: 678,
+// 	},
+	
+
+// })
