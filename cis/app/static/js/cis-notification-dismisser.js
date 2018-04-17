eba03b22476a7319dd8614272645ac9e0841fe84
JPy <jparis.py@gmail.com>
2018-04-17 11:47:24 +0200

    fixes bugs + add verbatim

--- cis/app/static/js/cis-notification-dismisser.js
+++ cis/app/static/js/cis-notification-dismisser.js
@@ -12,3 +12,17 @@ $(document).on('click', 'article > .message-header > button.delete', function() 
 	$(this).parent().addClass('is-hidden');
 	return false;
 });
+
+
+
+// TO RECHECK IN ORDER TO DEAL WITH SAFARI BUG WITH IFRAME DISPLAY
+
+function resizeIframe () {
+	var iframe_H = $("#iframe_card_body").height() ;
+	console.log("------- iframe_H : ", iframe_H) ;
+	$("iframe").height(iframe_H + "px") ;
+};
+
+// $(document).on("click", ". btn-feedback-iframe", function(){
+// 	resizeIframe()
+// }
