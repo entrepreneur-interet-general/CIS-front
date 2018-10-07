dc0d305c17ed182220129132eb285ece61c96172
David Bruant <bruant.d@gmail.com>
2018-10-07 20:29:19 +0200

    Create the NavBar Vue component

---
+++ cis/app/frontend/scripts/components/UserMenu.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="navbar-item is-hoverable has-dropdown ">                  
+        <a class="navbar-item" href="/preferences/user_infos">
+            <span class="icon has-text-primary">
+                <i class="far fa-2x fa-user-circle"></i>
+            </span>
+        </a>
+        <div class="navbar-dropdown is-boxed is-right">
+            
+            <a class="navbar-item" href="/preferences/user_infos">
+                <strong> {{ user.userName }} {{ user.userSurname }} </strong> 
+                <!--
+                &nbsp;
+                ( {{ user.userAuthLevel }} )
+                -->
+            </a>
+
+            <hr class="navbar-divider">
+            <a class="navbar-item" href="/preferences/user_infos">
+                <span class="icon">
+                    <i class="fas fa-cog"></i>
+                </span>
+                Vos informations
+            </a>
+
+            <hr class="navbar-divider">
+            <a class="navbar-item" href="/preferences/user_password">
+                <span class="icon">
+                    <i class="fas fa-unlock"></i>
+                </span>
+                Changer le mot de passe
+            </a>
+
+            <!--
+            {% if user_infos.userAuthLevel in ["admin", "staff"] %}
+            <hr class="navbar-divider">
+            {# <div 	class="navbar-item tooltip is-tooltip-bottom is-tooltip-warning is-tooltip-multiline"
+                    data-tooltip="espace d'administration du site réservé aux niveaux : staff et admin"
+                > #}
+                {# <a class="button is-warning is-outlined" href="/admin" target="_blank"> #}
+                <a class="navbar-item" href="/admin" target="_blank">
+                    <span class="icon">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                    Espace Admin
+                </a>
+            {# </div> #}
+            {% endif %}
+            -->
+
+            <a class="navbar-item" href="/logout">
+                <button class="button is-fullwidth">
+                    Se déconnecter
+                </button>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	props: ['user']
+}
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
+
+
+
+
+
--- cis/app/frontend/scripts/main.js
+++ cis/app/frontend/scripts/main.js
@@ -1,17 +1,22 @@
 import Vue from 'vue';
-import Brand from './components/Brand.vue';
+import NavBar from './components/NavBar.vue';
 
-console.info('Coucou !!')
-
-new Vue({
-    el: '#brand',
-    render: createElement => createElement(
-        Brand, 
-        {
-            props: {
-                logo: '/static/logos/CIS/CIS_beta_logo_LD.png',
-                brand: 'Carrefour des Innovations Sociales' 
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: document.querySelector('nav'),
+        render: createElement => createElement(
+            NavBar, 
+            {
+                props: {
+                    logo: '/static/logos/CIS/CIS_beta_logo_LD.png',
+                    brand: 'Carrefour des Innovations Sociales',
+                    user: {
+                        // TODO import user infos to the client-side
+                        userName: 'DAV BRU',
+                        userSurname: 'HARDCODED'
+                    }
+                }
             }
-        }
-    )
-})
+        )
+    })
+}, {once: true})  
