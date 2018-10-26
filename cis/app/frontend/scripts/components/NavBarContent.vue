9b46c62eb9bbe32959076ad0ead7ed1e4a304274
David Bruant <bruant.d@gmail.com>
2018-10-26 17:39:54 +0200

    Simplify navbar HTML

--- cis/app/frontend/scripts/components/NavBarContent.vue
+++ cis/app/frontend/scripts/components/NavBarContent.vue
@@ -1,38 +1,32 @@
 <template>
     <div id="navbarMain" class="navbar-menu">
+		<div class="navbar-end">
 
-        <div class="navbar-end">
-            <span class="navbar-item">
-
-                <a class="field is-grouped">
-					<div class="navbar-item">
-						<a class="button is-text" href="/bientot/recherche">
-							<span>Moteur de recherche</span>
-						</a>
-					</div>
-
-					<div class="navbar-item">
-						<a class="button is-text" href="/bientot/le-projet">
-							<span>Projet</span>
-						</a>
-					</div>
-
-					<div class="navbar-item">
-						<a class="button is-text" href="/bientot/qui-sommes-nous"> 
-							<span>Qui sommes-nous ?</span>
-						</a>
-					</div>
-
-					<div class="navbar-item">
-						<a class="button is-text" href="/bientot/nous-rejoindre"> 
-							<span>Nous rejoindre</span>
-						</a>
-					</div>
+			<div class="navbar-item">
+				<a class="button is-text" href="/bientot/recherche">
+					<span>Moteur de recherche</span>
 				</a>
+			</div>
 
-            </span>
-        </div>
-    </div>
+			<div class="navbar-item">
+				<a class="button is-text" href="/bientot/le-projet">
+					<span>Projet</span>
+				</a>
+			</div>
+
+			<div class="navbar-item">
+				<a class="button is-text" href="/bientot/qui-sommes-nous"> 
+					<span>Qui sommes-nous ?</span>
+				</a>
+			</div>
+
+			<div class="navbar-item">
+				<a class="button is-text" href="/bientot/nous-rejoindre"> 
+					<span>Nous rejoindre</span>
+				</a>
+			</div>
+
+		</div>
 </template>
 
 <script>
