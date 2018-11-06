ffd973d37a9c7cba92cf8f41e24abdc4c8018d93
Julien_P <jparis.py@gmail.com>
2018-11-06 01:02:09 +0000

    adding english pages (home + project pages) + fixing alphabetical order in sources + shuffle_seed

---
+++ cis/app/static/json/CHOICES_FILTERS_PARTNERS.js
@@ -0,0 +1,58 @@
+var CHOICES_FILTERS_PARTNERS = [
+    {
+        "fullname": "Sourceurs", 
+        "name": "sources_", 
+        "choices": [
+            {
+                "fullname": "AG2R la mondiale", 
+                "id": "5a9da08d0a82868f49cd3816", 
+                "name": "AG2R la mondiale"
+            }, 
+            {
+                "fullname": "Apriles", 
+                "id": "5aa3de630a828651b2ebfe3e", 
+                "name": "Apriles"
+            }, 
+            {
+                "fullname": "Avise", 
+                "id": "5a9d9fd80a82868f4973d2d3", 
+                "name": "Avise"
+            }, 
+            {
+                "fullname": "Bretagne Creative", 
+                "id": "5ab3f5fe0a8286585d3a307e", 
+                "name": "Bretagne Creative"
+            }, 
+            {
+                "fullname": "Fondation Daniel et Nina Carasso", 
+                "id": "5aabf8250a8286bf097cec82", 
+                "name": "Fondation Daniel et Nina Carasso"
+            }, 
+            {
+                "fullname": "Fondation Veolia", 
+                "id": "5aaff7360a82860b06c94a6d", 
+                "name": "Fondation Veolia"
+            }, 
+            {
+                "fullname": "Fondation Vinci", 
+                "id": "5aaff8aa0a82860b06c94a6e", 
+                "name": "Fondation Vinci"
+            }, 
+            {
+                "fullname": "MOT", 
+                "id": "5aaff93f0a82860b06c94a6f", 
+                "name": "MOT"
+            }, 
+            {
+                "fullname": "Semeoz", 
+                "id": "5aaffa390a82860b06c94a70", 
+                "name": "Semeoz"
+            }, 
+            {
+                "fullname": "My Positive Impact", 
+                "id": "5ab410c00a82863566d5fd05", 
+                "name": "My Positive Impact"
+            }
+        ]
+    }
+]
