e068a6b7c055e5e3071687c95dc202cd19f709c0
David Bruant <bruant.d@gmail.com>
2019-01-03 16:14:23 +0100

    Add visual toggle for french/english

--- cis/app/frontend/scripts/components/NavBarContent.vue
+++ cis/app/frontend/scripts/components/NavBarContent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="navbarMain" class="navbar-menu">
+    <div id="navbar-main" class="navbar-menu">
 		<div class="navbar-end">
 
 			<div class="navbar-item">
@@ -26,12 +26,10 @@
 				</a>
 			</div>
 
-
 			<div class="navbar-item">
-				<a class="button is-text" href="/en"> 
-					<span>English</span>
-				</a>
+				<span lang="fr">FR</span>&nbsp;/&nbsp;<a lang="en" href="/en">EN</a>
 			</div>
+
 		</div>
 	</div>
 </template>
